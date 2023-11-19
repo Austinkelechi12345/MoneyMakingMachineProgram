32dc9a717272e35b9440386ef4a649b1c3cf1aed
Austinkelechi12345 <98892833+Austinkelechi12345@users.noreply.github.com>
2023-11-19 08:44:19 -0800

    i started my media queries in my resonsive.css file

--- js/script.js
+++ js/script.js
@@ -11,7 +11,16 @@
 
 }
 
+// for my nav
+function myFunction(){
+    let x = document.getElementById("myLinks");
+    if(x.style.display === "block"){
+        x.style.display = "none";
 
+    } else{
+        x.style.display ="block"
+    }
+}
 
 
 

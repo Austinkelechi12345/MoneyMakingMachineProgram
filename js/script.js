caee133f2b1b15b91708b7ce3773af9bdb6f0882
Austinkelechi12345 <98892833+Austinkelechi12345@users.noreply.github.com>
2023-11-24 08:01:35 -0800

    i made some updates to my website and got done with my website

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
- function sendMail() {
-     let params = {
-         from_name: document.getElementById("fullName").value,
-         email_id: document.getElementById("email_id").value,
-         message: document.getElementById("message").value
-     }
-     emailjs.send("service_zdw0ehu", "template_555dgkr", params).then(function (res) {
-         alert("Thanks for reaching out to us!" + " " + res.status);
+function sendMail() {
+    let params = {
+        from_name: document.getElementById("fullName").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById("message").value
+    }
+    emailjs.send("service_zdw0ehu", "template_555dgkr", params).then(function (res) {
+        alert("Thanks for reaching out to us!" + " " + res.status);
 
-     });
+    });
 
 }
 
 // for my nav
-function myFunction(){
+function myFunction() {
     let x = document.getElementById("myLinks");
-    if(x.style.display === "block"){
+    if (x.style.display === "block") {
         x.style.display = "none";
 
-    } else{
-        x.style.display ="block"
+    } else {
+        x.style.display = "block"
     }
 }
+
+
+
+
 
 
 
